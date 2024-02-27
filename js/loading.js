8be30badd61e6c6071ab38b280170e8b78c0c1f0
Ayden860 <134655473+Ayden860@users.noreply.github.com>
2024-02-27 22:43:52 +0000

    Tue, Feb 27, 2024, 2:43 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,12 @@
 loadText = document.querySelector(".loading-text");
-
+bg = document.querySelector(".bg");
 load = 0;
 
-loadText.innerText = `${load}%`;
-loadText.style.opacity
+blurring();
+
+function blurring(){
+    load++;
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = 1;
+    bg.style.filter = "blur(10px)";
+}
