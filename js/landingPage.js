b9f04ba1c20a246c9099a794085f17096f87e170
Ayden860 <134655473+Ayden860@users.noreply.github.com>
2024-03-07 16:10:58 -0800

    Need to fix the modal

---
+++ js/landingPage.js
@@ -0,0 +1,26 @@
+console.log('hello')
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+// model may be modal for id
+modal = document.getElementById('modal')
+close = document.getElementById('close')
+
+
+// toggle Nav
+toggle.addEventListener('click', () =>{
+    document.body.classList.toggle('show-nav')
+})
+// Show modal
+open.addEventListener('click', () =>{
+    modal.classList.add('show-modal')
+})
+// Hide Modal
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+})
+// Hide model on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal){
+        modal.classList.remove('show-modal')
+    }
+})
